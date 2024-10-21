e271367fd9a2ef1b01e269ebcc601ccd444f32ec
minji <alswlchlrh8@naver.com>
2024-10-21 20:21:48 +0900

    Initial commit

---
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/TweetMyPetBackendApplication.java
@@ -0,0 +1,13 @@
+package tweet_my_pet.tweet_my_pet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetMyPetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetMyPetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tweet_my_pet/tweet_my_pet_backend/TweetMyPetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tweet_my_pet.tweet_my_pet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetMyPetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
