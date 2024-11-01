23c02f12c01511ff7575ac015a254a7ba99c73aa
minji <alswlchlrh8@naver.com>
2024-11-02 05:03:45 +0900

    [UPDATE] 회원가입 입력폼 유효성 검증 및 데이터 중복여부 검증 기능 구현

--- src/main/java/tweet_my_pet/tweet_my_pet_backend/service/UserService.java
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/service/UserService.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tweet_my_pet.tweet_my_pet_backend.dto.SignupRequestDto;
 import tweet_my_pet.tweet_my_pet_backend.entity.Users;
+import tweet_my_pet.tweet_my_pet_backend.exception.DuplicateResourceException;
 import tweet_my_pet.tweet_my_pet_backend.repository.UsersRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * 사용자 서비스
@@ -26,12 +31,65 @@ public class UserService {
      */
     @Transactional
     public Users signUp(SignupRequestDto signupRequestDto) {
+        // 중복 아이디 체크
+        if (usersRepository.existsByLoginId(signupRequestDto.getLoginId())) {
+            throw new DuplicateResourceException("이미 존재하는 아이디입니다.");
+        }
+
+        // 회원가입 요청 정보로 사용자 생성
         Users user = Users.builder()
                 .loginId(signupRequestDto.getLoginId())
                 .password(signupRequestDto.getPassword())
                 .name(signupRequestDto.getName())
                 .phoneNumber(signupRequestDto.getPhoneNumber())
                 .build();
+
         return usersRepository.save(user);
+    }
+
+    /**
+     * 전화번호 존재여부 확인
+     */
+    public void existsByPhoneNumber(String phoneNumber) {
+        if (usersRepository.existsByPhoneNumber(phoneNumber)) {
+            throw new DuplicateResourceException("이미 존재하는 전화번호입니다.");
+        }
+    }
+
+    /**
+     * 인증 관련
+     */
+    private Map<String, String> authCodeStore = new HashMap<>();
+
+    /**
+     * 인증 코드 생성
+     * @param phoneNumber
+     */
+    public String generateAuthCode(String phoneNumber) {
+        // 6자리 인증 코드 생성
+        String authCode = String.format("%06d", new Random().nextInt(999999));
+        authCodeStore.put(phoneNumber, authCode);
+        log.info("인증번호: {}", authCode);
+        return authCode;
+    }
+
+    /**
+     * 인증 코드 확인
+     * @param phoneNumber
+     * @param authCode
+     */
+    public boolean verifyAuthCode(String phoneNumber, String authCode) {
+        String key = "{\"phoneNumber\":\"" + phoneNumber + "\"}";
+        String storedAuthCode = authCodeStore.get(key);
+
+        // 인증번호가 동일하면 authCodeStore에서 지우고 true 반환
+        if (authCode.equals(storedAuthCode)) {
+            authCodeStore.remove(phoneNumber);
+            log.info("인증번호 확인 성공");
+            return true;
+        } else {
+            log.error("인증번호 확인 실패: 인증번호 불일치");
+            return false;
+        }
     }
 }
