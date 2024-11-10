a67e8aaca0d518a5f2f6f63f06b94954254f101a
Jinsu <kimjs991229@gmail.com>
2024-11-10 15:52:11 +0900

    entity 및 repository 수정

--- src/main/java/tweet_my_pet/tweet_my_pet_backend/repository/UsersRepository.java
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/repository/UsersRepository.java
@@ -2,7 +2,6 @@ package tweet_my_pet.tweet_my_pet_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import tweet_my_pet.tweet_my_pet_backend.entity.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
     // 아이디 중복 체크
