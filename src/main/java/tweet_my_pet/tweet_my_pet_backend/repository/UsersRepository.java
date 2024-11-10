03aadf419073c6a6ac2a0999bb5c947cac70b847
seokjin925 <153346471+seokjin925@users.noreply.github.com>
2024-11-10 16:27:24 +0900

    USERREPOSITORY 최신사항

--- src/main/java/tweet_my_pet/tweet_my_pet_backend/repository/UsersRepository.java
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/repository/UsersRepository.java
@@ -2,18 +2,19 @@ package tweet_my_pet.tweet_my_pet_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import tweet_my_pet.tweet_my_pet_backend.entity.Users;
+import tweet_my_pet.tweet_my_pet_backend.entity.User;
+import tweet_my_pet.tweet_my_pet_backend.entity.NoApiUserLogin;
 
-public interface UsersRepository extends JpaRepository<Users, Long> {
+public interface UsersRepository extends JpaRepository<NoApiUserLogin, Long> {
     // 아이디 중복 체크
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM Users u WHERE u.loginId = :loginId")
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM NoApiUserLogin u WHERE u.loginId = :loginId")
     boolean existsByLoginId(String loginId);
 
     // 전화번호 중복 체크
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM Users u WHERE u.phoneNumber = :phoneNumber")
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.userPhoneNumber = :phoneNumber")
     boolean existsByPhoneNumber(String phoneNumber);
 
     // 아이디로 사용자 조회
-    Users findByLoginId(String loginId);
-    Users findByKakaoId(String kakaoId);
+    NoApiUserLogin findByLoginId(String loginId);
+    NoApiUserLogin findByKakaoId(String userPhoneNumber);
 }
