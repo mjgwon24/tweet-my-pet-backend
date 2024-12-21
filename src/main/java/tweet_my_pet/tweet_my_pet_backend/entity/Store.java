d8710a185c483a96848e36aeb9a80e4e693767a1
minji <alswlchlrh8@naver.com>
2024-12-21 22:35:43 +0900

    feat: Store 엔티티 속성 추가 및 StoreFeature 엔티티 구현

--- src/main/java/tweet_my_pet/tweet_my_pet_backend/entity/Store.java
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/entity/Store.java
@@ -1,32 +1,40 @@
 package tweet_my_pet.tweet_my_pet_backend.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.locationtech.jts.geom.Point;
 
-@Getter
-@Setter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 @Table(name = "store")
 public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "store_id", nullable = false)
-    private Long storeId;
+    private Long id;
 
-    @Column(name = "store_name", nullable = false, length = 45)
+    @Column(nullable = false, length = 45)
     private String storeName;
 
-    @Column(name = "store_tel", nullable = false, length = 45)
+    @Column(nullable = false, length = 45)
     private String storeTel;
 
-    @Column(name = "store_location", nullable = false)
+    @Column(nullable = false)
     private String storeLocation;
 
-    @Column(name = "store_point", nullable = false, columnDefinition = "POINT")
-    private Point storePoint;
+    @Column(nullable = false, columnDefinition = "POINT")
+    private Point storePoint; // 위도, 경도
 
-    @Column(name = "store_president_name", nullable = true, length = 45)
+    @Column(length = 45)
     private String storePresidentName;
+
+    private String petGuide;
+    private String useGuide;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "store_introduce_id")
+    private StoreFeature storeFeature;
 }
---
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/entity/StoreFeature.java
@@ -0,0 +1,28 @@
+package tweet_my_pet.tweet_my_pet_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Table(name = "store_feature")
+public class StoreFeature {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_feature_id", nullable = false)
+    private Long id;
+
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private List<PetSizeType> petSizeTypes; // 수용 가능한 견종 사이즈 리스트 (대형, 중형, 소형)
+
+    private Boolean isParking;              // 주차 가능 여부
+    private Boolean isDogPark;              // 애견운동장 보유 여부
+    private Boolean isDogSwimmingPool;      // 애견수영장 보유 여부
+    private Boolean isInternet;             // 무선인터넷 가능 여부
+    private Boolean isBarbecue;             // 바비큐 시설 보유 여부
+}
