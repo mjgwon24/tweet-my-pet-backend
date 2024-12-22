16979a29c80e54f8c87c5c559d28b1a4b5a7c71b
minji <alswlchlrh8@naver.com>
2024-12-22 20:17:42 +0900

    feature: StoreRestController 매장 저장 api 구현

---
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/restController/StoreRestController.java
@@ -0,0 +1,28 @@
+package tweet_my_pet.tweet_my_pet_backend.restController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tweet_my_pet.tweet_my_pet_backend.dto.StoreDto.CreateStoreRequest;
+import tweet_my_pet.tweet_my_pet_backend.dto.StoreDto.FetchStoreResponse;
+import tweet_my_pet.tweet_my_pet_backend.dto.common.ResponseDto;
+import tweet_my_pet.tweet_my_pet_backend.service.StoreService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/store")
+public class StoreRestController {
+    private final StoreService storeService;
+
+    // 매장 저장
+    @PostMapping("/save")
+    public ResponseEntity<ResponseDto<FetchStoreResponse>> saveStore(@RequestBody CreateStoreRequest createStoreRequest) {
+        FetchStoreResponse fetchStoreResponse = storeService.createStore(createStoreRequest);
+        return new ResponseEntity<>(
+                new ResponseDto<>(ResponseDto.Status.SUCCESS, "매장 저장 성공", fetchStoreResponse),
+                HttpStatus.OK
+        );
+    }
+}
+
