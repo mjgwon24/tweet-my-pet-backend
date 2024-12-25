5b0e36a5edc9405a2e223118e71e6ac878a94a61
minji <alswlchlrh8@naver.com>
2024-12-25 21:48:13 +0900

    fix: StoreFeature 컬럼 추가에 따른 매장 특징 반환 로직 개선

--- src/main/java/tweet_my_pet/tweet_my_pet_backend/util/StoreFeatureUtil.java
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/util/StoreFeatureUtil.java
@@ -10,13 +10,20 @@ public class StoreFeatureUtil {
     static public String storeFeatureListToString(StoreFeature storeFeature) {
         List<String> features = new ArrayList<>();
 
-        storeFeature.getAcceptPetSizes().forEach(petSize -> features.add(petSize.toString()));
-
+        if (storeFeature.getIsSmallDog() && storeFeature.getIsMediumDog() && storeFeature.getIsLargeDog()) features.add("모든견종 가능");
+        else {
+            if (storeFeature.getIsSmallDog()) features.add("소형견");
+            if (storeFeature.getIsMediumDog()) features.add("중형견");
+            if (storeFeature.getIsLargeDog()) features.add("대형견");
+        }
         if (storeFeature.getIsParking()) features.add("주차장");
         if (storeFeature.getIsDogPark()) features.add("애견운동장");
         if (storeFeature.getIsDogSwimmingPool()) features.add("애견수영장");
         if (storeFeature.getIsInternet()) features.add("인터넷");
         if (storeFeature.getIsBarbecue()) features.add("바베큐");
+        if (storeFeature.getIsFoodPacking()) features.add("포장 가능");
+        if (storeFeature.getIsWaitingPlace()) features.add("대기 공간");
+        if (storeFeature.getIsKidSeat()) features.add("유아 의자");
 
         return String.join("·", features.stream().limit(3).toList());
     }
