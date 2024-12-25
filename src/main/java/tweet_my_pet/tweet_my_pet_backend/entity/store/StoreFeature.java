32ef612a2eab1877ce5b1494fc768cd4266034a7
seokjin925 <153346471+seokjin925@users.noreply.github.com>
2024-12-25 21:00:19 +0900

    feature: 매장 feature 정보 추가

--- src/main/java/tweet_my_pet/tweet_my_pet_backend/entity/store/StoreFeature.java
+++ src/main/java/tweet_my_pet/tweet_my_pet_backend/entity/store/StoreFeature.java
@@ -28,4 +28,8 @@ public class StoreFeature {
     private Boolean isDogSwimmingPool;      // 애견수영장 보유 여부
     private Boolean isInternet;             // 무선인터넷 가능 여부
     private Boolean isBarbecue;             // 바비큐 시설 보유 여부
+    private Boolean isToiletDivision;       // 남/녀 화장실 구분 여부
+    private Boolean isFoodPacking;          // 포장 가능 여부
+    private Boolean isWatingPlace;          // 대기 공간 존재 여부
+    private Boolean isKidSeat;              // 유아 의자 존재 여부
 }
